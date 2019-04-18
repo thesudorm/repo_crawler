b80b77cd5a6ab1ec3b691aa7779284364c1eed23
Joseph Bauer <bauerj@josephbauer.xyz>
2019-04-18 09:41:16 -0400

    Miner now only looks at source files.

--- main.py
+++ main.py
@@ -1,23 +1,43 @@
 from pydriller import * 
 from config import git_repo
 
+# CONSTANTS
+SRC_FILES = ["js", "c", "py", "rb"]
+
+# Globals
 counter = 0
+
+#Helper functions
+def IsSourceFile(filename):
+    is_src = False
+    if(filename.find(".") >= 0):
+        file_extention = m.filename.split(".")[1]
+        if file_extention in SRC_FILES:
+            is_src = True
+
+    return is_src
+
+
+
+## MAIN ###
 
 # Outer loop that steps through commits
 for commit in RepositoryMining(git_repo).traverse_commits():
     counter = counter + 1
     print("Commit " + str(counter) + "\n")
-    commit_code = ""
 
     # Inner loops that steps trough each modification in the current commit
     for m in commit.modifications:
-        print(m.filename + " " + str(m.change_type))
-        #print(m.source_code)
 
-        # In here, we can parse the code for style changes.
-        # was thinking that instead of parsing the same text over and over again,
-        # that we instead look at the modifications. With each modification, we can
-        # add or subtract the number of times the author uses a certain style
-        # (number of times they put '{' on the same line as 'if', # of times tabs is used, etc)
+        if IsSourceFile(m.filename): #Determine if the file is a source file or not
+            print(m.filename + " " + str(m.change_type))
+            #print(m.source_code)
+
+            # In here, we can parse the code for style changes.
+            # was thinking that instead of parsing the same text over and over again,
+            # that we instead look at the modifications. With each modification, we can
+            # add or subtract the number of times the author uses a certain style
+            # (number of times they put '{' on the same line as 'if', # of times tabs is used, etc)
 
     print()
+
