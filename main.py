b62f62af4ae3968abc1ca697545812e818d3ee3f
Joseph Bauer <bauerj@josephbauer.xyz>
2019-04-27 21:38:24 -0400

    Started learning minidoc parsing library. Also, added code to track number of lines and avg line length per commit.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # Other libraries
 from pydriller import * 
 import subprocess
-import xml.etree.ElementTree as ET
+
+from xml.dom import minidom
+from xml.parsers.expat import ExpatError
 
 # Our imports
 from config import git_repo
@@ -12,7 +14,9 @@ SRC_FILES = ["js", "c", "py", "rb"]
 # Globals
 counter = 0
 
-#Helper functions
+#### Helper functions
+
+#Takes a file name as an input and determines if it is a source file or not
 def IsSourceFile(filename):
     is_src = False
     if(filename.find(".") >= 0):
@@ -30,27 +34,57 @@ def GetSRCML(source, filetype):
 
 ## MAIN ###
 
+# Variables for tracking entire project
+prj_num_of_lines        = 0
+prj_total_line_length   = 0
+
+gr = GitRepository(git_repo)
+
 # Outer loop that steps through commits
 for commit in RepositoryMining(git_repo).traverse_commits():
     counter = counter + 1
-    print("Commit " + str(counter) + "\n")
 
     # Inner loops that steps trough each modification in the current commit
     for m in commit.modifications:
+        if IsSourceFile(m.filename): 
 
-        if IsSourceFile(m.filename): #Determine if the file is a source file or not
-            print(m.filename + " " + str(m.change_type))
+            # Calculate line length
+            parsed_lines = gr.parse_diff(m.diff)
+            added = parsed_lines['added']
+            deleted = parsed_lines['deleted']
+
+            for x in added:
+                if(x[1] != ''):
+                    prj_num_of_lines += 1
+                    prj_total_line_length += len(x[1])
+
+            for x in deleted:
+                if(x[1] != ''):
+                    prj_num_of_lines -= 1
+                    prj_total_line_length -= len(x[1])
+
+            #srcml stuff
             xml_string = GetSRCML(m.source_code, 'C')
-    
-            xml = ET.fromstring(xml_string)
+            xml = minidom.parseString(xml_string)
 
-            for x in xml.findall('.//{http://www.srcML.org/srcML/src}if'):
-                print(x.tag, x.attrib)
+            unit = xml.documentElement
+            ifs = unit.getElementsByTagName("if")
 
+            #for x in ifs:
+                #for child in x.childNodes:
+                    #if(child.nodeType != child.TEXT_NODE):
+                        #print(child.tagName)
+                    #if(child.nodeType == child.TEXT_NODE):
+                        #print(child.nodeValue)
+                    
             # In here, we can parse the code for style changes.
             # was thinking that instead of parsing the same text over and over again,
             # that we instead look at the modifications. With each modification, we can
             # add or subtract the number of times the author uses a certain style
             # (number of times they put '{' on the same line as 'if', # of times tabs is used, etc)
 
+
+    print("Commit " + str(counter) + "\n")
+    print("LOC: " + str(prj_num_of_lines))
+    print("Average Line Length: " + str(prj_total_line_length / prj_num_of_lines))
     print()
